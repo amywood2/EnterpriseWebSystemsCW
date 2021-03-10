daa2dd27c190debb553708ff37f9a2b5b0288d66
amywood2 <a.wood12@rgu.ac.uk>
2021-03-10 15:09:24 +0000

    adding heroku

--- server/server.js
+++ server/server.js
@@ -11,6 +11,13 @@ mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
 
+if (proccess.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'Home.js'));
+  })
+}
+
 app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
