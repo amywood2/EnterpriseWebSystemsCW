2c43af28cbccb59e8f67eca5d804c8dc033445e2
amywood2 <a.wood12@rgu.ac.uk>
2021-03-14 12:46:53 +0000

    adding carousel items

--- client/core/Home.js
+++ client/core/Home.js
@@ -37,15 +37,69 @@ class Carousel extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-
+      items: [
+        {
+          id: 0,
+          title: 'Project One',
+          subTitle: 'Project Title',
+          //imgSrc: image,
+          link: 'http://google.com',
+          selected: false
+        },
+        {
+          id: 2,
+          title: 'Project two',
+          subTitle: 'Project Title',
+          //imgSrc: image,
+          link: 'http://google.com',
+          selected: false
+        },
+        {
+          id: 3,
+          title: 'Project three',
+          subTitle: 'Project Title',
+          //imgSrc: image,
+          link: 'http://google.com',
+          selected: false
+        },
+      ]
     }
   }
+
+  handleCardClick = (id, card) => {
+
+    let items = [...this.state.items];
+
+    items[id].selected = items[id].selected ? false : true;
+
+    items.forEach(item => {
+        if(item.id !== id) {
+            item.selected = false;
+        }
+    });
+
+    this.setState({
+        items
+    });
+  }
+
+  makeItems = (items) => {
+        return items.map(item => {
+            return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+
+
   render() {
     return (
-      <p> Carousel works </p>
-    )
-  }
-}
+      <Container fluid={true}>
+                <Row className="justify-content-around">
+                    {this.makeItems(this.state.items)}
+                </Row>
+            </Container>
+        );
+      }
+    }
 
 export default function Home(){
   const classes = useStyles()
