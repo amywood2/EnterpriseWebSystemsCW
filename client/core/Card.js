a7d9c50e48993e592f24bbf156029706d6794511
amywood2 <a.wood12@rgu.ac.uk>
2021-03-14 16:02:13 +0000

    styling

--- client/core/Card.js
+++ client/core/Card.js
@@ -6,7 +6,7 @@ import CardInfo from './../core/CardInfo';
 function Card(props) {
 
     return(
-        <div className="d-inline-block g-card" onClick={(e) => props.click(props.item)}>
+        <div onClick={(e) => props.click(props.item)}>
             <img style= {{"border-radius": "10px", width: "100px", height: "100px", "box-shadow": "0px 0px 3px 1px #ccc"}} src={props.item.imgSrc} alt={props.item.imgSrc} />
             { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} /> }
         </div>
