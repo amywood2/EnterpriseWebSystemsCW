8c70c0dafcd9ec806a9cd4eda4328df864f34e35
amywood2 <a.wood12@rgu.ac.uk>
2021-03-14 15:32:19 +0000

    styling image

--- client/core/Card.js
+++ client/core/Card.js
@@ -8,13 +8,13 @@ const useStyles = makeStyles(theme => ({
     height: "10px",
     margin: "15px"
   }
-}))
+})
 
 function Card(props) {
 
     return(
         <div className="d-inline-block g-card" onClick={(e) => props.click(props.item)}>
-            <img className={classes.cardImage} src={props.item.imgSrc} alt={props.item.imgSrc} />
+            <img style= {{width: "10px", height: "10px"}} src={props.item.imgSrc} alt={props.item.imgSrc} />
             { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} /> }
         </div>
     );
