808683487d42af725f54a940fd00a6903b404262
amywood2 <a.wood12@rgu.ac.uk>
2021-03-15 10:58:56 +0000

    attempting to add view button

--- client/core/CardInfo.js
+++ client/core/CardInfo.js
@@ -10,6 +10,9 @@ function CardInfo(props) {
             <p style={{"font-size":"20px", margin: "0px"}}>{props.title}</p>
             <p style={{"font-size":"16px", margin: "0px"}}>{props.subTitle}</p>
             <a href={props.link} target="_blank" rel="noopener noreferrer">View</a>
+            <Link to={"/user/" + auth.isAuthenticated().user._id}>
+              <Button style={isActive(history, "/user/" + auth.isAuthenticated().user._id)}>My Profile</Button>
+            </Link>
         </animated.div>
     );
 
